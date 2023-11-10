0a5dba709431a3643e43189cc380a15d0c12e3f9
Anousone M. <anousone.mounivongs@gmail.com>
2023-11-10 02:56:29 +0100

    première partie ok

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>TP React Développeur Web et Web Mobile</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Rechercher des films</a>
       </header>
     </div>
   );
