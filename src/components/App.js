47e7b79cb5b08228e75a9f250c453a7f9057dc4f
Anousone M. <anousone.mounivongs@gmail.com>
2023-11-10 10:40:13 +0100

    partie 3 done : rotation css ok

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,9 @@ import './App.css';
 import Home from './home/Home';
 
 function App() {
-  const clicBouton = () => alert('bouton cliqué')
   return (
     <header className="App">
       <Home />
-      <button
-        className="App-button"
-        onClick={clicBouton}>
-        Modifier l’animation</button>
     </header>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import logo from '../logo.svg';
 import './Home.css';
 
 
-const Home = () => {
+function Home() {
+
+    const [animation, setAnimation] = useState(true)
+    const toggleAnimation = () => {
+        setAnimation(animation => !animation)
+    }
+
     return (
         <div className="container">
-            <img src={logo} className="logo" alt="logo" />
+            <img src={logo} className={`logo ${animation && "rotate"}`} alt="logo" />
             <h1>TP React Développeur Web et Web Mobile</h1>
+            <button
+                className="App-button"
+                onClick={toggleAnimation}>
+                Modifier l’animation
+                </button>
         </div>
     )
 }
